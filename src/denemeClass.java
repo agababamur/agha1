ead9e630272e66a914387c175104049c140fbf0f
Aghababa Mursaliyev <aghababa1986@gmail.com>
2020-05-20 13:44:48 -0700

    deneme eklendi deyisiklerle

--- src/denemeClass.java
+++ src/denemeClass.java
@@ -1,6 +1,5 @@
 public class denemeClass {
     public static void main(String[] args) {
-        System.out.println ("Hello world");
-        
+        System.out.println ("ello  salam word");
     }
 }
